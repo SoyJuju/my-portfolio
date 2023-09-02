733273b8b24d7c67461f8b6f2e10c9035b7a2211
SoyJuju <julianthant@gmail.com>
2023-09-02 15:32:37 +0630

    commit 18.0

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,35 @@
 import '../../styles/main.css';
 
+import twitter_logo from '../../assets/icon-twitter.svg';
+import leetcode_logo from '../../assets/icon-leetcode.svg';
+import mail_logo from '../../assets/icon-mail.svg';
+
 export default function ContactMe() {
   return (
     <section className="contact | container">
+      <div className="contact--introduction">
+        <h1 className="contact-heading | fs-small-950 fw-bold">
+          Let's chat. <br />
+          Tell me about your project.
+        </h1>
+        <p className="contact--subintro | fs-small-600 fw-medium padding-block-300">
+          Let's create something together.
+        </p>
+        <div className="contact--email-container">
+          <img className="contact--icon" src={mail_logo} alt="" />
+          <div>
+            <h3 className="contact--email-heading | fs-smsll-400 fw-bold">
+              Mail me at
+            </h3>
+            <a
+              href="mailto:julianthant@gmail.com"
+              className="contact--email | fw-regular fs-small-200"
+            >
+              julianthant@gmail.com
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="contact--information | text-primary-400 bg-neutral-100">
         <p className="contact--text | fs-small-200">GET IN TOUCH</p>
         <h1 className="contact-heading | fs-primary-heading fw-black">
@@ -48,7 +75,7 @@ export default function ContactMe() {
           <input
             type="hidden"
             name="_next"
-            value="https://julianhein.netlify.app/"
+            value="https://julianhein.netlify.app/contact"
           ></input>
           <button type="submit" className="contact--submit | square-button">
             Send
