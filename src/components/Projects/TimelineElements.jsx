c1f61d22452f85117e1f153f3b6c6b27e3e713f0
SoyJuju <julianthant@gmail.com>
2023-09-14 00:18:57 +0630

    car showroom project

--- src/components/Projects/TimelineElements.jsx
+++ src/components/Projects/TimelineElements.jsx
@@ -1,10 +1,11 @@
 import MyPortfolioWebsite from '../../assets/my-portfolio.png';
 import CoffeeWebsite from '../../assets/coffee-website.png';
 import ToDoListWebsite from '../../assets/todolist-website.png';
+import CarShowroom from '../../assets/dad-showroom.png';
 
 let timelineElements = [
   {
-    id: 3,
+    id: 1,
     title: 'To Do List',
     url: 'https://juju-to-do-list.netlify.app',
     description:
@@ -14,8 +15,8 @@ let timelineElements = [
     image: ToDoListWebsite,
   },
   {
-    id: 4,
-    title: 'Main Portfolio Website',
+    id: 2,
+    title: 'Portfolio Website',
     url: 'https://julianhein.netlify.app',
     description:
       'The portfolio website I currently use. This website is built with Astro, React and SCSS which is more complex compared to the first portfolio website. The website structure and theme is similar, but the main difference is the frameworks. The coding dashboard is also linked with both codewars and leetcode apis to get the latest updates on my profile. The basic version was <a href="http://julianhein.great-site.net" target="_blank">http://julianhein.great-site.net</a>.',
@@ -24,14 +25,24 @@ let timelineElements = [
     image: MyPortfolioWebsite,
   },
   {
-    id: 5,
-    title: 'Main Coffee Website',
+    id: 3,
+    title: 'Coffee Website',
     url: 'https://baristachawsu.netlify.app',
     description:
       'This is an improved version of the first coffee website I made for Barista Chaw Su. This improved version is built upon Astro, React and SCSS. The design is quite similar but the frameworks used are different. The basic one was <a href="http://baristachawsu.great-site.net" target="_blank">http://baristachawsu.great-site.net</a>.',
     date: '2023',
     tag: 'Educational',
     image: CoffeeWebsite,
+  },
+  {
+    id: 4,
+    title: 'Car Showroom Website',
+    url: 'https://stylenation.netlify.app',
+    description:
+      "This is the website I made for my dad's business. He opens a car showroom so I made him a website for his showroom. The website shows the cars he sells and ways to contact him.",
+    date: '2023',
+    tag: 'Business',
+    image: CarShowroom,
   },
 ];
 
